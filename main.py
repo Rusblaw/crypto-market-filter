ded109da8e37bda9676f5c36e235aa4a99dd3e8c
Rusblaw <blazhyyevsky.r2@gmail.com>
2026-06-14 23:09:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 PHONE = os.getenv("PHONE_NUMBER")
 LOGIN_CODE = os.getenv("LOGIN_CODE", "")
+PHONE_CODE_HASH = os.getenv("PHONE_CODE_HASH", "")
 
 client = TelegramClient(StringSession(), API_ID, API_HASH)
-
 client.connect()
 
 if not client.is_user_authorized():
     if LOGIN_CODE == "":
-        client.send_code_request(PHONE)
+        sent = client.send_code_request(PHONE)
         print("CODE_SENT")
+        print("PHONE_CODE_HASH:")
+        print(sent.phone_code_hash)
     else:
-        client.sign_in(PHONE, LOGIN_CODE)
+        client.sign_in(phone=PHONE, code=LOGIN_CODE, phone_code_hash=PHONE_CODE_HASH)
         print("SESSION_STRING:")
         print(client.session.save())
 else:
